f3b6a7ee14612607f3a2184996bc3c86b1fb8693
Tiago Santos <tiago40427@gmail.com>
2025-10-29 04:34:36 -0300

    feat(Skills):Adding a new grid template

--- src/components/Skills/index.tsx
+++ src/components/Skills/index.tsx
@@ -1,9 +1,24 @@
+import clsx from "clsx";
 import { Heading } from "../Heading";
+import { GridCard } from "./GridCard";
 
-export function  Skills() {
-  return(
-   <section className="p-2 flex flex-col" >
+export function Skills() {
+  return (
+    <section className="p-2 flex flex-col ">
+      <Heading text="Minhas Skills" />
 
-   </section>
-  )
+      <div
+        className={clsx(
+          "grid grid-cols-3 grid-rows-3 gap-2 my-4",
+          "[grid-template-areas:'div1_div1_div2'_'div3_div4_div4'_'div5_div5_div5']"
+        )}
+      >
+        <GridCard className="h-60" gridArea="div1" />
+        <GridCard gridArea="div2" />
+        <GridCard gridArea="div3" />
+        <GridCard gridArea="div4" />
+        <GridCard gridArea="div5" />
+      </div>
+    </section>
+  );
 }
