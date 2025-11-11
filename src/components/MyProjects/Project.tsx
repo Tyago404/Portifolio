95949c9142844efed837a869b1e60097cbd28777
Tiago Santos <tiago40427@gmail.com>
2025-11-11 01:10:19 -0300

    feat(projects):Added projects images&texts

--- src/components/MyProjects/Project.tsx
+++ src/components/MyProjects/Project.tsx
@@ -1,6 +1,7 @@
+"use client";
 import Image from "next/image";
-import { Heading } from "../Heading";
 import { Paragraph } from "../Paragraph";
+import { motion } from "motion/react";
 
 type ProjectProps = {
   src: string;
@@ -11,8 +12,8 @@ type ProjectProps = {
 
 export function Project({ src, alt, paragraphText, titleText }: ProjectProps) {
   return (
-    <div className="border">
-      <div className="relative h-100 w-50 ">
+    <div className="border w-full">
+      <div className="relative h-100 brightness-80 hover:brightness-40 transition-all ">
         <Image src={src} alt={alt} fill className="object-cover" />
       </div>
       <div className=" flex flex-col gap-2 p-2 bg-black ">
--- src/components/MyProjects/index.tsx
+++ src/components/MyProjects/index.tsx
@@ -5,30 +5,30 @@ export function MyProjects() {
   return (
     <section>
       <Heading as="h1" text="Meus Projetos" />
-      <div className="flex">
+      <div className="flex ">
         <Project
           src="/images/chronos-pomodoro-print.png"
           alt="chronosPrint"
           titleText="Chronos Pomodoro"
-          paragraphText="App web pra gerenciar o tempo com a técnica Pomodoro."
+          paragraphText="App web Pomodoro para gerenciar ciclos de foco de forma simples e intuitiva, com layout einterface limpa."
         />
         <Project
-          src="/images/chronos-pomodoro-print.png"
-          alt="chronosPrint"
-          titleText="Chronos Pomodoro"
-          paragraphText="App web pra gerenciar o tempo com a técnica Pomodoro. "
+          src="/images/riffly-print.png"
+          alt="rifflyPrint"
+          titleText="Riffly"
+          paragraphText="E-commerce responsivo desenvolvido com Next.js, TailwindCSS e design system."
         />
         <Project
-          src="/images/chronos-pomodoro-print.png"
-          alt="chronosPrint"
-          titleText="Chronos Pomodoro"
-          paragraphText="App web pra gerenciar o tempo com a técnica Pomodoro. "
+          src="/images/blog-print.png"
+          alt="blogPrint"
+          titleText="The Blog"
+          paragraphText="Blog para treinar consultas usando Drizzle, com fluxo simples e organizado de conteúdo."
         />
         <Project
-          src="/images/chronos-pomodoro-print.png"
-          alt="chronosPrint"
-          titleText="Chronos Pomodoro"
-          paragraphText="App web pra gerenciar o tempo com a técnica Pomodoro. "
+          src="/images/hotel-print.png"
+          alt="HotelPrint"
+          titleText="Hotel"
+          paragraphText="Site de hotel desenvolvido com Next.js e TailwindCSS, layout responsivo e visual moderno."
         />
       </div>
     </section>
