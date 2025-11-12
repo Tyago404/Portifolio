9900d825f760bf5297466c9b0244b5a10ac306ba
Tiago Santos <tiago40427@gmail.com>
2025-11-12 00:40:17 -0300

    feat(about):Adding social media links

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { Paragraph } from "../Paragraph";
 import { Facebook, GithubIcon, Instagram, Linkedin } from "lucide-react";
+import Link from "next/link";
 
 export function About() {
   return (
@@ -19,10 +20,34 @@ export function About() {
         <Paragraph text="Sou um profissional em transição de carreira, saindo do ramo musical para seguir meu verdadeiro interesse: o desenvolvimento Front-End. Tenho ampla experiência em trabalho em equipe e no contato direto com clientes, adquirida durante meu tempo como balconista em loja de música." />
         <Paragraph text="Sou uma pessoa comunicativa, com facilidade e vontade constante de aprender. Tenho experiência com Next.js e TailwindCSS, e busco minha primeira oportunidade na área para continuar evoluindo e contribuir com projetos que unam tecnologia, design e boa experiência do usuário." />
         <div className="flex gap-4">
-          <Facebook size={30} />
-          <Instagram size={30} />
-          <Linkedin size={30} />
-          <GithubIcon size={30} />
+          <Link
+            className="hover:scale-101 hover:brightness-75 transition-all duration-300"
+            target="_blank"
+            href={"https://www.linkedin.com/in/tiago-santos-53761b179/"}
+          >
+            <Linkedin size={30} />
+          </Link>
+          <Link
+            className="hover:scale-101 hover:brightness-75 transition-all duration-300"
+            target="_blank"
+            href={"https://github.com/Tyago404"}
+          >
+            <GithubIcon size={30} />
+          </Link>
+          <Link
+            className="hover:scale-101 hover:brightness-75 transition-all duration-300"
+            target="_blank"
+            href={"https://www.facebook.com/tiago.santos.256554"}
+          >
+            <Facebook size={30} />
+          </Link>
+          <Link
+            className="hover:scale-101 hover:brightness-75 transition-all duration-300"
+            target="_blank"
+            href={"https://www.instagram.com/tyago404/"}
+          >
+            <Instagram size={30} />
+          </Link>
         </div>
       </div>
     </section>
