af882580f9c56e378bece7b00fba9481c9db9c69
Tiago Santos <tiago40427@gmail.com>
2025-11-18 00:12:59 -0300

    style(about):adding mobile style

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -5,18 +5,21 @@ import Link from "next/link";
 
 export function About() {
   return (
-    <section className="flex w-full border border-transparent border-b-white py-8">
-      <Image
-        src="/images/profile-icon.png"
-        alt="profile"
-        width={400}
-        height={300}
-        priority
-        className="bg-white m-6"
-      />
+    <section className="flex flex-col items-center md:flex-row w-full border border-transparent border-b-white py-8 justify-center">
+      <div className=" relative w-full max-w-100 aspect-square bg-white p-2 rounded-full">
+        <Image
+          src="/images/profile-icon.png "
+          alt="profile"
+          fill
+          priority
+          className="bg-blue-200 object-cover w-full h-full rounded-full "
+        />
+      </div>
 
-      <div className="flex flex-col gap-6 justify-center w-[50%] m-6">
-        <h1 id="about" className="text-5xl font-bold ">Sobre mim </h1>
+      <div className="flex flex-col gap-6 justify-center w-full md:max-w-[50%] m-6">
+        <h1 id="about" className="text-5xl font-bold ">
+          Sobre mim
+        </h1>
         <Paragraph text="Sou um profissional em transição de carreira, saindo do ramo musical para seguir meu verdadeiro interesse: o desenvolvimento Front-End. Tenho ampla experiência em trabalho em equipe e no contato direto com clientes, adquirida durante meu tempo como balconista em loja de música." />
         <Paragraph text="Sou uma pessoa comunicativa, com facilidade e vontade constante de aprender. Tenho experiência com Next.js e TailwindCSS, e busco minha primeira oportunidade na área para continuar evoluindo e contribuir com projetos que unam tecnologia, design e boa experiência do usuário." />
         <div className="flex gap-4">
